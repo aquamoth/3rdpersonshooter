6ad3fc478694521accc34e17401487a969c2de20
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-01 12:31:00 +0200

    Basic movement of soldier

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private float moveSpeed= 100;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal"); 
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime*moveSpeed);
+    }
+}
