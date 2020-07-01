10c5db297c9927680484095b47481a2162e21cfd
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-01 17:52:27 +0200

    Can shoot environment

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField, Range(0.5f, 1.5f)] private float fireRate = 1f;
+    [SerializeField, Range(1, 10)] private int damage = 1;
+    [SerializeField] private Transform firePoint;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= fireRate)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+
+    private void FireGun()
+    {
+        Debug.DrawRay(firePoint.position, firePoint.forward * 100, Color.red, 2f);
+
+        var ray = new Ray(firePoint.position, firePoint.forward);
+        if (Physics.Raycast(ray, out RaycastHit hitinfo, 100f))
+        {
+            Destroy(hitinfo.collider.gameObject);
+        }
+
+    }
+}
