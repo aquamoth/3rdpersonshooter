7c349d9e60b3567b2d989a1f1190f424be1b0649
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-01 17:02:22 +0200

    Turning with animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private Animator animator;
+    
     [SerializeField] private float moveSpeed= 100;
+    [SerializeField] private float turnSpeed = 5f;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -20,5 +24,12 @@ public class PlayerMovement : MonoBehaviour
         var movement = new Vector3(horizontal, 0, vertical);
 
         characterController.SimpleMove(movement * Time.deltaTime*moveSpeed);
+        animator.SetFloat("Speed", movement.magnitude);
+
+        if (movement.magnitude>0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
     }
 }
