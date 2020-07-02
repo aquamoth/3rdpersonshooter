f98bbfd6cdf22cefb8183411e9f737a705028716
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-02 09:00:31 +0200

    Enemy is chasing me when in proximity

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+    private AggroDetection aggroDetection;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        aggroDetection = GetComponent<AggroDetection>();
+        aggroDetection.OnAggro += AggroDetection_OnAggro;
+    }
+
+    private void AggroDetection_OnAggro(Transform target)
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void Update()
+    {
+        var speed = navMeshAgent.velocity.magnitude;
+        animator.SetFloat("Speed", speed);
+
+    }
+}
