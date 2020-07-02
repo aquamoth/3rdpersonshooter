01cf9c82170310c7d963d8b8249b0bd14dd6afb7
Mattias Åslund <mattias.aslund@shoppa.com>
2020-07-02 08:02:20 +0200

    Added backwards movement animation using mixamo.com

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
     private Animator animator;
-    
-    [SerializeField] private float moveSpeed= 100;
-    [SerializeField] private float turnSpeed = 5f;
+
+    [SerializeField] private float forwardMoveSpeed = 7.5f;
+    [SerializeField] private float backwardMoveSpeed = 3f;
+    [SerializeField] private float turnSpeed = 150f;
 
     private void Awake()
     {
@@ -18,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal"); 
+        var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
         var movement = new Vector3(horizontal, 0, vertical);
@@ -26,22 +27,10 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", vertical);
         transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
 
-        if (vertical!=0)
+        if (vertical != 0)
         {
+            var moveSpeed = vertical > 0 ? forwardMoveSpeed : backwardMoveSpeed;
             characterController.SimpleMove(transform.forward * moveSpeed * vertical);
         }
-
-
-
-
-
-        //characterController.SimpleMove(movement * Time.deltaTime*moveSpeed);
-        //animator.SetFloat("Speed", movement.magnitude);
-
-        //if (movement.magnitude>0)
-        //{
-        //    Quaternion newDirection = Quaternion.LookRotation(movement);
-        //    transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
-        //}
     }
 }
